1c1c1d16ad785bd143bcd4e5721e4482e666f070
Sawyer Cherry <sawyeracherry@gmail.com>
2018-10-09 09:52:33 -0500

    Initial Commit

---
+++ Assesment 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assesment 1
+//
+//  Created by Sawyer Cherry on 10/9/18.
+//  Copyright © 2018 Sawyer Cherry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
